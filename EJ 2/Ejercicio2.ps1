2c03de351da4a37afb4642660af89d65a5b2f4b2
EzequielCorno <ezequiel.corno1996@gmail.com>
2018-04-11 21:16:30 -0300

    EJ1 y paginas de EJ2

---
+++ EJ 2/Ejercicio2.ps1
@@ -0,0 +1,4 @@
+﻿$html = "F:\EZEQUIEL\Sistemas Operativos\Trabajo Practico 1 - Escobar, Corno, Pereira, Buzzoni, Rossi\EJ 2\paginas\PáginaWeb.html"
+
+$a = (get-content $html | convertTo-Html > at.html).parsedhtml.title
+echo $a
