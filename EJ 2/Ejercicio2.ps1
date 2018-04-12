0b76b99c9d9cab08e8a9f894683c706164571f63
EzequielCorno <ezequiel.corno1996@gmail.com>
2018-04-11 23:12:33 -0300

    script del ej2 casi terminado

--- EJ 2/Ejercicio2.ps1
+++ EJ 2/Ejercicio2.ps1
@@ -1,4 +1,26 @@
-﻿$html = "F:\EZEQUIEL\Sistemas Operativos\Trabajo Practico 1 - Escobar, Corno, Pereira, Buzzoni, Rossi\EJ 2\paginas\PáginaWeb.html"
+﻿$prepo = 'a','ante','bajo','cabe','con','contra','de','desde','durante','en','entre','hacia','hasta','mediante','para','por','sin','so','sobre','tras','versus','vía'
+$URI = “http://www.sisop.com.ar/“
+$HTML = Invoke-WebRequest -Uri $URI
+#[HTMLFile] $html = Get-Content -Path "C:\Users\Ariel\OneDrive\Documentos\mipagina.html"
 
-$a = (get-content $html | convertTo-Html > at.html).parsedhtml.title
-echo $a
+$palabras = $HTML.ParsedHtml.title.Split(' ')
+echo $palabras.count
+$CantPag = 0
+$cantpal = @{}
+#While(paginas)
+$i = 0
+while($i -lt $palabras.count){
+    if(!$prepo.Contains($palabras[$i])){
+        #si esta sumar
+        if($cantpal.ContainsKey($palabras[$i])){
+            $cantpal[$palabras[$i]]+=1
+        }else{
+            #Si no está, agregar
+            $cantpal.add($palabras[$i], 1)
+        }
+    }
+    $i++
+}
+echo "Páginas Analizadas:` $CantPag"
+echo 'Resultados Obtenidos:'
+echo $cantpal | Sort-Object -Property value -Descending
